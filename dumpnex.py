4b6fa22969ea5a62c84d17cf7ffc02ee67b10da0
MikeDX <mikedx@gmail.com>
2023-07-14 13:36:10 +0100

    Added comments to dumpnex file

--- dumpnex.py
+++ dumpnex.py
@@ -1,3 +1,76 @@
+"""
+NEX File Parser
+
+This script parses a NEX file and extracts the relevant information from its header and memory banks. The NEX file is a binary format used for storing data related to the ZX Spectrum Next computer.
+
+The script defines the structure of the NEX file format using the Construct library. It reads the NEX file, parses its contents, and provides access to the extracted data.
+
+Usage:
+    1. Ensure the Construct library is installed (pip install construct).
+    2. Update the "filename" variable with the path to your NEX file.
+    3. Run the script to parse the NEX file.
+    4. Extracted information is available in the "parsed_file" object.
+
+The script currently extracts the file header information, including the number of banks to load, loading screen flags, border color, stack pointer, program counter, and the list of memory banks included in the file. Additionally, it separates the memory banks into individual arrays of 16,384 bytes each.
+
+Due to the way construct works, it is also possible to modify the contents of the NEXT file and export the modified file.
+
+Note: This script assumes a specific structure for the NEX file format. Make sure your NEX files conform to this structure.
+
+Author: MikeDX (https://github.com/MikeDX) assisted by ChatGPT (https://chat.openai.com/)
+
+The NEX file format is used for storing various graphics and memory bank data for the ZX Spectrum Next computer. This header provides essential information about the file structure and contents.
+
+Header Structure:
+
+- "Next" string: A 4-byte identifier indicating the file type.
+
+- Version: A 4-byte string specifying the NEX file version (e.g., "V1.0", "V1.1", "V1.2", "V1.3").
+
+- RAM Required: Indicates the amount of RAM required by the program (0 = 768KB, 1 = 1792KB).
+
+- Number of 16K Banks to Load: Specifies the number of 16KB banks to load (0-112). The actual banks to load are defined in the "Banks Included" array.
+
+- Loading-Screen Blocks: A set of bit-flags specifying the presence of loading screens for different graphics modes, such as Layer2, ULA, Hi-Res, Lo-Res, and more.
+
+- Border Color: The border color index (0-7) for the graphics screens.
+
+- Stack Pointer: The initial stack pointer value.
+
+- Program Counter: The initial program counter value (0 = don't run, just load).
+
+- Number of Extra Files: Currently unused by any known software.
+
+- Banks Included: An array of 112 bytes indicating which 16KB memory banks are included in the file. A value of 1 at a specific index indicates that the corresponding bank is included.
+
+- Layer2 Loading Bar: Specifies whether the Layer2 loading bar is enabled (0 = OFF, 1 = ON).
+
+- Loading Bar Color: The color index (0-255) for the loading bar (for specific graphics modes).
+
+- Loading Delay per Bank: The amount of delay (in frames) between loading each bank.
+
+- Start Delay: The initial delay (in frames) before starting the loading process.
+
+- Preserve Current Next-Registers Values: Specifies whether to preserve the current Next-Registers values (0 = reset machine state, 1 = preserve).
+
+- Required Core Version: Specifies the required core version for the NEX file to run.
+
+- Timex HiRes 512x192 Mode Color: The color index for Timex HiRes 512x192 mode or the palette offset for Layer 2 320x256x8bpp or 640x256x4bpp modes.
+
+- Entry Bank: The bank to be mapped into slot 3 (0xC000-0xFFFF address space) after loading. Used for setting the program counter and file handle address.
+
+- File Handle Address: The file handle address. Determines how the NEX loader handles the file after loading is complete.
+
+- Extensions (V1.3 and beyond): Additional fields included in V1.3 and later versions of the NEX format.
+
+- Memory Banks: Raw memory bank data stored in 16KB chunks. The number of banks included depends on the "Number of 16K Banks to Load" field.
+
+Note: The NEX file format is extensible, and additional fields may be present in specific versions or for custom usage.
+
+
+"""
+
+
 from construct import *
 
 # Define the structure of the file header for v1.3
