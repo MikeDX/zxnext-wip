59b24086c3d032ba5ab663cafd62d7616c79bfff
MikeDX <mikedx@gmail.com>
2023-07-14 12:04:13 +0100

    Cleaned up cmake files

--- cmake/next.cmake
+++ cmake/next.cmake
@@ -17,3 +17,14 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+
+# Override object file extension
+set(CMAKE_C_OUTPUT_EXTENSION ".o")
+set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+
+
+# Override Clang-related variables
+set(CMAKE_C_COMPILER_TARGET "")
+set(CMAKE_OSX_ARCHITECTURES "")
+set(CMAKE_OSX_SYSROOT "")
